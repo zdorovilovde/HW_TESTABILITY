684cd42957d706011ed081588ccce4ab6ba713a1
zdorovilov test <zdorovilovde@mail.ru>
2024-05-03 21:16:45 +0500

    Calculate it works

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
+        int price = 6_666;
         int miles = service.calculate(price); // должно получиться 500
         System.out.println(miles);
     }
